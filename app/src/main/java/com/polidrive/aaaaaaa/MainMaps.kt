3071d391470c600ce39d59297b2df4efa9974719
Unlived <noessmurf23@gmail.com>
2024-01-09 20:41:57 -0600

    Ya hago rutas :)

--- app/src/main/java/com/polidrive/aaaaaaa/MainMaps.kt
+++ app/src/main/java/com/polidrive/aaaaaaa/MainMaps.kt
@@ -1,5 +1,6 @@
 package com.polidrive.aaaaaaa
 
+import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
@@ -12,7 +13,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.PolylineOptions
@@ -24,6 +24,7 @@ import com.google.maps.android.compose.rememberCameraPositionState
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -74,18 +75,23 @@ fun MapScreen(onMarkerAdded: (LatLng) -> Unit) {
     }
     Button(
         onClick = {
+            Log.d("RouteData", "Jala el boton")
             // Implementar la lógica para crear la ruta usando los marcadores seleccionados
+            createRoute { newRoutePoints ->
+                routePoints = newRoutePoints}
             if (selectedMarkers.size >= 3) {
                 start = "${selectedMarkers[1].longitude},${selectedMarkers[1].latitude}"
                 end = "${selectedMarkers[2].longitude},${selectedMarkers[2].latitude}"
                 // Aquí iría la lógica para crear y mostrar la ruta
-
+                createRoute { newRoutePoints ->
+                    routePoints = newRoutePoints}
             }
+
         },
         colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF880B56))
     ) {
         Text(text = "Dios Ayudame:(")
-        routePoints = createRoute()
+        //routePoints = createRoute()
     }
     GoogleMap(
         modifier = Modifier.fillMaxSize(),
@@ -103,30 +109,52 @@ fun MapScreen(onMarkerAdded: (LatLng) -> Unit) {
                 snippet = ""
 
             )
-            Polyline(points = routePoints)
+            Polyline(points = listOf(escom, conductor, pasajero))
+            Polyline(points = routePoints,
+                color = Color.Green)
         }
     }
 }
 
 
-fun createRoute():List<LatLng>{
-    CoroutineScope(Dispatchers.IO).launch{
-        val call = getRetrofit().create(ApiService::class.java).getRoute("5b3ce3597851110001cf6248dab664ccc8a543caa4beefa356a82bc8","","")
-        if(call.isSuccessful){
-            drawRoute(call.body())
-        }else{
+fun createRoute(updateRoutePoints: (List<LatLng>) -> Unit) {
+    Log.d("RouteData", "Paso sin acento")
+    CoroutineScope(Dispatchers.IO).launch {
+        val escom1 = LatLng(19.504841431557495, -99.14630154688206)
+        val conductor2 = LatLng(19.60154401792762, -99.04025319106024)
+        val call = getRetrofit().create(ApiService::class.java).getRoute("5b3ce3597851110001cf6248dab664ccc8a543caa4beefa356a82bc8","${escom1.longitude},${escom1.latitude}", "${conductor2.longitude},${conductor2.latitude}")
 
+        Log.d("RouteData", "Paso sin acento 2")
+
+        if (call.isSuccessful) {
+            Log.d("RouteData", "Paso sin acento 3")
+            val responseBody = call.body()
+            Log.d("RouteData", "API Response: $responseBody")
+            val newRoutePoints = mutableListOf<LatLng>()
+            call.body()?.features?.first()?.geometry?.coordinates?.forEach {
+
+
+                val latLng = LatLng(it[1], it[0])
+                newRoutePoints.add(latLng)
+                Log.d("RouteData", "Route points: xd")
+            }
+            withContext(Dispatchers.Main) {
+                updateRoutePoints(newRoutePoints)
+            }
+        } else {
+            Log.e("RouteData", "API Call Failed: ${call.errorBody()}")
+            // Manejo de error
         }
     }
-    return emptyList()
 }
+
 
 fun drawRoute(routeResponse: RouteResponse?) {
     val polylineOptions = PolylineOptions()
     routeResponse?.features?.first()?.geometry?.coordinates?.forEach{
         //polylineOptions.add(LatLng(it[1], it[0]))
         //Polyline(points = LatLng(it[1], it[0]))
-        
+
     }
 
 }
@@ -137,12 +165,4 @@ fun getRetrofit(): Retrofit {
         .baseUrl("https://api.openrouteservice.org/")
         .addConverterFactory(GsonConverterFactory.create())
         .build()
-}
-
-
-
-@Preview
-@Composable
-fun MapSrceenPreview(){
-    MainMapScreen()
 }
--- app/src/main/java/com/polidrive/aaaaaaa/RouteResponse.kt
+++ app/src/main/java/com/polidrive/aaaaaaa/RouteResponse.kt
@@ -4,4 +4,4 @@ import com.google.gson.annotations.SerializedName
 
 data class RouteResponse (@SerializedName("features")val features:List<Features>)
 data class Features(@SerializedName("geometry")val geometry:Geometry)
-data class Geometry(@SerializedName("coodinates") val coordinates:List<List<Double>>)
+data class Geometry(@SerializedName("coordinates") val coordinates:List<List<Double>>)
