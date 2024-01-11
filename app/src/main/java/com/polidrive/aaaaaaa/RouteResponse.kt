e7c9e41ed48521d357cbd77b5c7acc7c084d7df7
Unlived <noessmurf23@gmail.com>
2024-01-11 00:55:20 -0600

    Rutas al 100 de conductor-escuela y conductor-pasajero

--- app/src/main/java/com/polidrive/aaaaaaa/RouteResponse.kt
+++ app/src/main/java/com/polidrive/aaaaaaa/RouteResponse.kt
@@ -3,5 +3,7 @@ package com.polidrive.aaaaaaa
 import com.google.gson.annotations.SerializedName
 
 data class RouteResponse (@SerializedName("features")val features:List<Features>)
-data class Features(@SerializedName("geometry")val geometry:Geometry)
+data class Features(@SerializedName("geometry")val geometry:Geometry, @SerializedName("properties")val properties:Properties)
 data class Geometry(@SerializedName("coordinates") val coordinates:List<List<Double>>)
+data class Properties(@SerializedName("summary") val summary:Summary)
+data class Summary (@SerializedName("distance")val distance:Float, @SerializedName("duration")val duration:Float)
--- app/src/main/java/com/polidrive/aaaaaaa/RutaConductor.kt
+++ app/src/main/java/com/polidrive/aaaaaaa/RutaConductor.kt
@@ -1,7 +1,6 @@
 package com.polidrive.aaaaaaa
 
 import android.util.Log
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
@@ -16,7 +15,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.PolylineOptions
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
@@ -31,28 +29,36 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 
 @Composable
-fun MainMapScreen() {
+fun DriverMap () {
     var selectedMarkers by remember { mutableStateOf(listOf<LatLng>()) }
     var start:String=""
     var end:String=""
     Column {
         // ... tu UI existente ...
 
+        Button(
+            onClick = {
+                // Implementar la lógica para crear la ruta usando los marcadores seleccionados
+                if (selectedMarkers.size >= 3) {
+                    start = "${selectedMarkers[1].longitude},${selectedMarkers[1].latitude}"
+                    end = "${selectedMarkers[2].longitude},${selectedMarkers[2].latitude}"
+                    // Aquí iría la lógica para crear y mostrar la ruta
 
-
-        Box() {
-            MapScreen(onMarkerAdded = { latLng ->
-                selectedMarkers = listOf(latLng) + selectedMarkers.take(2)
-
-
-            })
+                }
+            },
+            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF880B56))
+        ) {
+            Text(text = "Crear ruta")
         }
 
+        MapDriverScreen(onMarkerAdded = { latLng ->
+            selectedMarkers = listOf(latLng) + selectedMarkers.take(2)
+        })
     }
 }
 
 @Composable
-fun MapScreen(onMarkerAdded: (LatLng) -> Unit) {
+fun MapDriverScreen(onMarkerAdded: (LatLng) -> Unit) {
     var selectedMarkers by remember { mutableStateOf(listOf<LatLng>()) }
     var start:String=""
     var end:String=""
@@ -69,75 +75,65 @@ fun MapScreen(onMarkerAdded: (LatLng) -> Unit) {
     var kilometros: Float = 0f
     var minutos: Float = 0f
 
+    Button(
+        onClick = {
+            Log.d("RouteData", "Jala el boton")
+            // Implementar la lógica para crear la ruta usando los marcadores seleccionados
+            createDriverRoute (
+                { newRoutePoints ->
+                    routePoints = newRoutePoints
+                },
+                { newDistancia -> kilometros = newDistancia },
+                { newTime -> minutos = newTime }
+            )
 
-    Column {
-
-
-        Button(
-            onClick = {
-                Log.d("RouteData", "Jala el boton")
-                // Implementar la lógica para crear la ruta usando los marcadores seleccionados
-                createRoute (
-                    { newRoutePoints ->
-                        routePoints = newRoutePoints
-                    },
-                    { newDistancia -> kilometros = newDistancia },
-                    { newTime -> minutos = newTime }
-                )
-
-
-            },
-            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF880B56))
-        ) {
-            Text(text = "Dios Ayudame:(")
-            //routePoints = createRoute()
+        },
+        colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF880B56))
+    ) {
+        Text(text = "Dios Ayudame:(")
+        //routePoints = createRoute()
+    }
+    GoogleMap(
+        modifier = Modifier.fillMaxSize(),
+        cameraPositionState = cameraPositionState,
+        onMapLongClick = { latLng ->
+            // Llama a la función de callback cada vez que se agrega un nuevo marcador
+            onMarkerAdded(latLng)
         }
-        GoogleMap(
-            modifier = Modifier.fillMaxSize(),
-            cameraPositionState = cameraPositionState,
-            onMapLongClick = { latLng ->
-                // Llama a la función de callback cada vez que se agrega un nuevo marcador
-                onMarkerAdded(latLng)
-            }
-        ) {
-            // Marcadores predefinidos
-            listOf(escom, conductor, pasajero).forEach { location ->
+    ) {
+        // Marcadores predefinidos
+        listOf(escom, conductor, pasajero).forEach { location ->
+            Marker(
+                state = MarkerState(position = location),
+                title = "Marcador",
+                snippet = ""
+
+            )
+            Polyline(points = listOf(escom, conductor, pasajero))
+
+            Polyline(points = routePoints,
+                color = Color.Green)
+
+            minutos /= 60
+            kilometros /= 1000
+            if(routePoints.size>0) {
                 Marker(
-                    state = MarkerState(position = location),
-                    title = "Marcador",
-                    snippet = ""
-
+                    state = MarkerState(position = routePoints[routePoints.size / 2]),
+                    title = "Distancia y tiempo",
+                    snippet = "$kilometros km. / $minutos min. "
                 )
-                Polyline(points = listOf(escom, conductor, pasajero))
-                Log.d("RouteData", "polyline 1")
-
-                Polyline(
-                    points = routePoints,
-                    color = Color.Green
-                )
-
-                minutos /= 60
-                kilometros /= 1000
-                Log.d("RouteData", "antes del Marker")
-                if(routePoints.size>0) {
-                    Marker(
-                        state = MarkerState(position = routePoints[routePoints.size / 2]),
-                        title = "Distancia y tiempo",
-                        snippet = "$kilometros km. / $minutos min. "
-                    )
-                }
             }
         }
     }
 }
 
 
-fun createRoute(updateRoutePoints: (List<LatLng>) -> Unit, newDistancia: (Float) -> Unit, newTime: (Float) -> Unit) {
+fun createDriverRoute(updateRoutePoints: (List<LatLng>) -> Unit, newDistancia: (Float) -> Unit, newTime: (Float) -> Unit) {
     Log.d("RouteData", "Paso sin acento")
     CoroutineScope(Dispatchers.IO).launch {
-        val escom1 = LatLng(19.504841431557495, -99.14630154688206)
+        val escom1 = LatLng(19.570988301429786, -99.06338216354723)
         val conductor2 = LatLng(19.60154401792762, -99.04025319106024)
-        val call = getRetrofit().create(ApiService::class.java).getRoute("5b3ce3597851110001cf6248dab664ccc8a543caa4beefa356a82bc8","${escom1.longitude},${escom1.latitude}", "${conductor2.longitude},${conductor2.latitude}")
+        val call = getDriverRetrofit().create(ApiService::class.java).getRoute("5b3ce3597851110001cf6248dab664ccc8a543caa4beefa356a82bc8","${escom1.longitude},${escom1.latitude}", "${conductor2.longitude},${conductor2.latitude}")
 
         Log.d("RouteData", "Paso sin acento 2")
 
@@ -176,18 +172,9 @@ fun createRoute(updateRoutePoints: (List<LatLng>) -> Unit, newDistancia: (Float)
 }
 
 
-fun drawRoute(routeResponse: RouteResponse?) {
-    val polylineOptions = PolylineOptions()
-    routeResponse?.features?.first()?.geometry?.coordinates?.forEach{
-        //polylineOptions.add(LatLng(it[1], it[0]))
-        //Polyline(points = LatLng(it[1], it[0]))
-
-    }
-
-}
 
 
-fun getRetrofit(): Retrofit {
+fun getDriverRetrofit(): Retrofit {
     return Retrofit.Builder()
         .baseUrl("https://api.openrouteservice.org/")
         .addConverterFactory(GsonConverterFactory.create())
